3a179cfd1d323abb2f7efa14920746b182821de3
Brent Mitton <bmitton@gatech.edu>
2020-06-17 09:52:11 -0400

    takes in strapi url

--- src/tina-strapi-client.ts
+++ src/tina-strapi-client.ts
@@ -3,9 +3,17 @@ import axios from "axios";
 export const STRAPI_JWT = "tina_strapi_jwt";
 export const STRAPI_URL = "http://localhost:1337";
 
+interface TinaStrapiClientOptions {
+  strapiUrl: string;
+}
 export class TinaStrapiClient {
+  strapiUrl: string;
+
+  constructor({ strapiUrl }: TinaStrapiClientOptions) {
+    this.strapiUrl = strapiUrl;
+  }
   async authenticate(username: string, password: string) {
-    return axios.post(`${STRAPI_URL}/auth/local`, {
+    return axios.post(`${this.strapiUrl}/auth/local`, {
       identifier: username,
       password: password,
     });
