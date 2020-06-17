74bf200c22c0e79ea78a590aa92b6fd876e3623f
Brent Mitton <bmitton@gatech.edu>
2020-06-17 10:12:10 -0400

    Makes more sense to pass URL to media store

--- src/strapi-media.store.ts
+++ src/strapi-media.store.ts
@@ -1,18 +1,25 @@
 import { Media, MediaStore, MediaUploadOptions } from "@tinacms/media";
-import { STRAPI_JWT, TinaStrapiClient } from "./tina-strapi-client";
 
 import Cookies from "js-cookie";
+import { STRAPI_JWT } from "./tina-strapi-client";
 import axios from "axios";
-import { useCMS } from "tinacms";
 
+interface StrapiMediaStoreOptions {
+  strapiUrl: string;
+}
 export class StrapiMediaStore implements MediaStore {
+  strapiUrl: string;
   accept = "*";
+
+  constructor({ strapiUrl }: StrapiMediaStoreOptions) {
+    this.strapiUrl = strapiUrl;
+  }
 
   async persist(files: MediaUploadOptions[]): Promise<Media[]> {
     const uploaded: Media[] = [];
 
     for (const { file } of files) {
-      const upload = await uploadFile(file);
+      const upload = await uploadFile(file, this.strapiUrl);
       uploaded.push({
         directory: "/uploads",
         filename: upload.data[0].hash + upload.data[0].ext,
@@ -23,21 +30,15 @@ export class StrapiMediaStore implements MediaStore {
   }
 }
 
-export async function uploadFile(file: File) {
-  const cms = useCMS();
-  const strapi: TinaStrapiClient = cms.api.strapi;
+export async function uploadFile(file: File, strapiUrl: string) {
   const authToken = Cookies.get(STRAPI_JWT);
   const formData = new FormData();
   formData.append("files", file);
-  const uploadResponse = await axios.post(
-    strapi.strapiUrl + "/upload",
-    formData,
-    {
-      headers: {
-        "Content-Type": "multipart/form-data",
-        Authorization: `Bearer ${authToken}`,
-      },
-    }
-  );
+  const uploadResponse = await axios.post(strapiUrl + "/upload", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${authToken}`,
+    },
+  });
   return uploadResponse;
 }
